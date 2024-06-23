c366c09ef00b31904d1c60fe31e124ca0075b16f
Fran Salanova <fsalanova.16@gmail.com>
2024-06-23 18:08:10 -0300

    Setting up initial client-server connection

--- client/room.js
+++ client/room.js
@@ -1,3 +1,30 @@
-import { io } from 'socket.io-client'
+import { io } from "socket.io-client"
 
-const socket = io('http://localhost:3000/')
+const production = process.env.NODE_ENV === "production"
+const serverURL = production ? "realsite.com" : "http://localhost:3000"
+
+const URLParams = new URLSearchParams(window.location.search)
+const name = URLParams.get("name")
+const roomId = URLParams.get("room-id")
+
+if (!name || !roomId) window.location = "/index.html"
+
+const socket = io(serverURL)
+
+const guessForm = document.querySelector("[data-guess-form]")
+const guessInput = document.querySelector("[data-guess-input]")
+const canvas = document.querySelector("[data-canvas]")
+const wordElement = document.querySelector("[data-word]")
+const messagesElement = document.querySelector("[data-messages]")
+const readyButton = document.querySelector("[data-ready-btn]")
+
+socket.emit("join-room", { name: name, roomId: roomId })
+endRound()
+
+function endRound() {
+  hide(guessForm)
+}
+
+function hide(element) {
+  element.classList.add("hide")
+}
--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
-const io = require('socket.io')(3000, {
+const production = process.env.NODE_ENV === "production"
+const clientURL = production ? "realsite.com" : "http://localhost:1234"
+
+const io = require("socket.io")(3000, {
   cors: {
-    origin: 'http://localhost:1234',
+    origin: clientURL,
   },
 })
 
-io.on('connection', (socket) => {
-  console.log('here')
+const rooms = {}
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (data) => {
+    const user = { id: socket.id, name: data.name, socket: socket }
+    let room = rooms[data.roomId]
+    if (room == null) {
+      room = { users: [], id: data.roomId }
+      rooms[data.roomId] = room
+    }
+
+    room.users.push(user)
+    socket.join(room.id)
+
+    socket.on("disconnect", () => {
+      room.users = room.users.filter((u) => u !== user)
+    })
+  })
 })
